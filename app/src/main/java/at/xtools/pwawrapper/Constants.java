d0e2c92ed0146e7b1a89a0010c6c84eca350762c
Daniil <coolswood@yandex.ru>
2020-09-03 17:38:00 +0300

    change colors

--- app/src/main/java/at/xtools/pwawrapper/Constants.java
+++ app/src/main/java/at/xtools/pwawrapper/Constants.java
@@ -3,8 +3,8 @@ package at.xtools.pwawrapper;
 public class Constants {
     public Constants(){}
     // Root page
-    public static String WEBAPP_URL = "https://www.leasingrechnen.at/";
-    public static String WEBAPP_HOST = "leasingrechnen.at"; // used for checking Intent-URLs
+    public static String WEBAPP_URL = "https://mind-healthy.ru/";
+    public static String WEBAPP_HOST = "mind-healthy.ru"; // used for checking Intent-URLs
 
 	// User Agent tweaks
     public static boolean POSTFIX_USER_AGENT = true; // set to true to append USER_AGENT_POSTFIX to user agent
@@ -18,7 +18,7 @@ public class Constants {
     // show your app when the page is loaded XX %.
     // lower it, if you've got server-side rendering (e.g. to 35),
     // bump it up to ~98 if you don't have SSR or a loading screen in your web app
-    public static int PROGRESS_THRESHOLD = 65;
+    public static int PROGRESS_THRESHOLD = 35;
     // turn on/off mixed content (both https+http within one page) for API >= 21
     public static boolean ENABLE_MIXED_CONTENT = true;
 }
