8937fe2c5a8083ea8ecf7758ca6b858eae785243
Daniil <coolswood@yandex.ru>
2020-09-06 10:07:00 +0300

    correct

--- app/src/main/java/at/xtools/pwawrapper/Constants.java
+++ app/src/main/java/at/xtools/pwawrapper/Constants.java
@@ -3,7 +3,7 @@ package at.xtools.pwawrapper;
 public class Constants {
     public Constants(){}
     // Root page
-    public static String WEBAPP_URL = "https://mind-healthy.ru/";
+    public static String WEBAPP_URL = "https://mind-health.ru/";
     public static String WEBAPP_HOST = "mind-healthy.ru"; // used for checking Intent-URLs
 
 	// User Agent tweaks
