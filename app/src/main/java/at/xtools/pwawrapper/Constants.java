6947fc08cfdd37f66caf92a623c1802ddafdfca4
Daniil <coolswood@yandex.ru>
2020-09-20 09:59:05 +0300

    version

--- app/src/main/java/at/xtools/pwawrapper/Constants.java
+++ app/src/main/java/at/xtools/pwawrapper/Constants.java
@@ -3,7 +3,7 @@ package at.xtools.pwawrapper;
 public class Constants {
     public Constants(){}
     // Root page
-    public static String WEBAPP_URL = "https://mind-health.ru/";
+    public static String WEBAPP_URL = "https://mind-healthy.ru/";
     public static String WEBAPP_HOST = "mind-healthy.ru"; // used for checking Intent-URLs
 
 	// User Agent tweaks
@@ -18,7 +18,7 @@ public class Constants {
     // show your app when the page is loaded XX %.
     // lower it, if you've got server-side rendering (e.g. to 35),
     // bump it up to ~98 if you don't have SSR or a loading screen in your web app
-    public static int PROGRESS_THRESHOLD = 0;
+    public static int PROGRESS_THRESHOLD = 100;
     // turn on/off mixed content (both https+http within one page) for API >= 21
     public static boolean ENABLE_MIXED_CONTENT = true;
 }
